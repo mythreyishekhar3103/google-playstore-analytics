b3f53d95957b0f99f6d26e6a98e46726ce9dc6d2
Mythreyi S <mythreyishekhar3103@gmail.com>
2026-06-04 11:42:37 +0530

    Update 5_AI_Insights.py

--- pages/5_AI_Insights.py
+++ pages/5_AI_Insights.py
@@ -1,1 +1,42 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/Google-Playstore.csv")
+
+st.title("🤖 Smart Insights")
+
+highest = df.loc[
+    df["Maximum Installs"].idxmax()
+]
+
+st.success(
+f"""
+Most Installed App:
+{highest['App Name']}
+
+Category:
+{highest['Category']}
+
+Installs:
+{highest['Maximum Installs']:,}
+"""
+)
+
+top_category = (
+    df["Category"]
+    .value_counts()
+    .idxmax()
+)
+
+st.info(
+f"Most Popular Category: {top_category}"
+)
+
+avg_rating = round(
+    df["Rating"].mean(),
+    2
+)
+
+st.warning(
+f"Average Play Store Rating: {avg_rating}"
+)

c3cd42bce994d1144fd7d6426e76d7696229e1d5
Mythreyi S <mythreyishekhar3103@gmail.com>
2026-06-04 13:27:06 +0530

    Update 2_Category_Analysis.py

--- pages/2_Category_Analysis.py
+++ pages/2_Category_Analysis.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data/Google-Playstore.csv")
+df = pd.read_csv("googleplaystore.csv")
 
 st.title("📊 Category Analysis")
 

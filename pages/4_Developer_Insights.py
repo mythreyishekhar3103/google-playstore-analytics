f11d3119a995e00b206a0ce59f4e29f636e77720
Mythreyi S <mythreyishekhar3103@gmail.com>
2026-06-04 13:27:48 +0530

    Fix case sensitivity in CSV file path

--- pages/4_Developer_Insights.py
+++ pages/4_Developer_Insights.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data/Google-Playstore.csv")
+df = pd.read_csv("data/googleplaystore.csv")
 
 st.title("👨‍💻 Developer Insights")
 

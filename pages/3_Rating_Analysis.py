e3e745c45b0d0020c9ed5cfc6c33ef110a14b0e0
Mythreyi S <mythreyishekhar3103@gmail.com>
2026-06-04 11:41:53 +0530

    Update 3_Rating_Analysis.py

--- pages/3_Rating_Analysis.py
+++ pages/3_Rating_Analysis.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/Google-Playstore.csv")
+
+st.title("⭐ Rating Analysis")
+
+avg_rating = (
+    df.groupby("Category")["Rating"]
+    .mean()
+    .sort_values(ascending=False)
+    .head(20)
+)
+
+fig = px.bar(
+    avg_rating,
+    title="Highest Rated Categories"
+)
+
+st.plotly_chart(fig, use_container_width=True)
